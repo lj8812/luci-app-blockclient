2bd3554268857bc0b73ad3a03ec19a5a3d7f1add
xxtt8812 <105150612+xxtt8812@users.noreply.github.com>
2025-02-26 21:19:50 +0800

    Update blockclient.lua

--- root/usr/lib/lua/luci/model/cbi/blockclient.lua
+++ root/usr/lib/lua/luci/model/cbi/blockclient.lua
@@ -1,6 +1,11 @@
-m = Map("blockclient", translate("客户端上网控制"), translate("通过IP或MAC地址禁止客户端上网"))
+m = Map("blockclient", translate("客户端上网控制"), 
+    translate("实时生效配置，无需重启即可阻断网络连接"))
 
-s = m:section(TypedSection, "rule", translate("禁止规则"))
+m.on_after_commit = function(self)
+    os.execute("/etc/init.d/blockclient reload >/dev/null 2>&1")
+end
+
+s = m:section(TypedSection, "rule", translate("阻断规则"))
 s.addremove = true
 s.anonymous = true
 
